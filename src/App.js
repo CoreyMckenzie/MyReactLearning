c36cec707639cdb93a29882a25e8ba1a0a25cca2
Corey Mckenzie <84005339+CoreyMckenzie@users.noreply.github.com>
2022-11-23 05:31:14 +0000

    Using .map to iterate over an array that contains objects to create multiple html elements in lesss lines

--- src/App.js
+++ src/App.js
@@ -8,12 +8,15 @@ class App extends Component {
     this.state = {
       monsters: [{
         name: 'Frankenstein',
+        id: 'asc1'
       },
       {
         name: 'Dracula',
+        id: 'asc2'
       },
       {
         name: 'Zombie',
+        id: 'asc3'
       }
       ]
     };
@@ -24,10 +27,11 @@ class App extends Component {
     return (
       <div className='App'>
         {
-          this.state.monsters.map((monster) => {
-            
+          this.state.monsters.map((monster) => { // map() is a built-in JS function that takes an array and returns a new array with the same number of elements as the original array
+            return <h1 key={monster.id}>{monster.name}</h1>
           })
         }
+
       </div>
     );
   } 
