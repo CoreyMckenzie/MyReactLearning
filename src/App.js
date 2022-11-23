ef1389c694409cf39eb633b71745f656cb7445f1
Corey Mckenzie <84005339+CoreyMckenzie@users.noreply.github.com>
2022-11-23 05:19:11 +0000

    Using the array method .map, it allows you to iterate over all elements in  the array, and gives you back a new array.

--- src/App.js
+++ src/App.js
@@ -4,31 +4,30 @@ import { Component } from 'react';
 class App extends Component {
   constructor() {
     super();
+
     this.state = {
-      planet: 'Earth',
-      lifeforms: 'humans'
+      monsters: [{
+        name: 'Frankenstein',
+      },
+      {
+        name: 'Dracula',
+      },
+      {
+        name: 'Zombie',
+      }
+      ]
     };
+
   }
 
   render() {
     return (
-      <div>
-        <h1>Hello World, {this.state.planet}. These {this.state.lifeforms} live here</h1>
-        <button 
-          onClick={() => {
-            this.setState(() => {
-              return {
-                planet: 'Mars',
-                lifeforms: 'Martians'
-              };
-            }, () => {
-              console.log(this.state);
-            }); //Passing a function + a callback function to setState. 
-            console.log(this.state);
-          }}
-        >
-          Click to change state
-        </button>
+      <div className='App'>
+        {
+          this.state.monsters.map((monster) => {
+            
+          })
+        }
       </div>
     );
   } 
