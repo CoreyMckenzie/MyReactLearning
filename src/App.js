571cc208df0475a38b9ad87b611cfa4cad07a4e0
Corey Mckenzie <84005339+CoreyMckenzie@users.noreply.github.com>
2022-11-23 04:55:01 +0000

    shallow merge

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@
 import { Component } from 'react';
 
 class App extends Component {
-
+  constructor() {
+    super();
+    this.state = {
+      planet: 'Earth',
+      lifeforms: 'humans'
+    };
+  }
 
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
-        <button>
+        <h1>Hello World, {this.state.planet}. These {this.state.lifeforms} live here</h1>
+        <button 
+          onClick={() => {
+            this.setState({ planet: 'Mars' }); //Shallow merge of local state
+            console.log(this.state);
+          }}
+        >
           Click to change state
         </button>
       </div>
